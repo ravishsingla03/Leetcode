87715ea0183979e5bcd3b2e15c68a9163af231ba
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2023-10-15 11:32:58 +0530

    Time: 4 ms (20.71%) | Memory: 6.9 MB (21.19%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -16,9 +16,26 @@ class Solution {
         dp[tar] = ans;
         return dp[tar];
     }
+
+    
+    int solveTab(vector<int>& nums, int tar){
+        vector<unsigned int>dp(tar+1,0);
+        dp[0]=1;
+        for(int i=1;i<=tar;i++){
+            for(int j=0;j<nums.size();j++){
+                if( i-nums[j] >=0){
+                    dp[i] += dp[i-nums[j]];
+                }
+            }
+        }
+        return dp[tar];
+    }
+
+    
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int>dp(target+1,-1);
-        return solve(nums,target,dp);
+        // vector<int>dp(target+1,-1);
+        // return solve(nums,target,dp);
+        return solveTab(nums,target);
     }
 };
