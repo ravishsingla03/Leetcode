7e88993416892eee16da5050c7a761107a88021d
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2023-10-30 08:11:38 +0530

    Time: 15 ms (15.69%) | Memory: 13.6 MB (5.36%) - LeetSync

---
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    int solve(int x){
+        int count=0;
+        while(x>0){
+            if(x&1){
+                count++;
+            }
+            x=x>>1;
+        }
+        return count;
+    }
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        vector<vector<int>>v;
+        for(int i=0;i<arr.size();i++){
+            int x = solve(arr[i]);
+            v.push_back({x,arr[i]});
+        }
+        sort(v.begin(),v.end());
+        vector<int>ans;
+        for(auto i: v){
+            ans.push_back(i[1]);
+        }
+        return ans;
+    }
+};
