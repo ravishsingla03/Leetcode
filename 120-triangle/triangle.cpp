f5fab295dbee6e0f8c9abc3bf8fc04afb9117282
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2023-11-26 15:33:37 +0530

    Time: 4 ms (75.96%) | Memory: 9.8 MB (5.41%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -9,12 +9,25 @@ class Solution {
         dp[i][j]  = triangle[i][j] + min(solve(triangle,i+1,j,dp),solve(triangle,i+1,j+1,dp)); 
         return dp[i][j];
     }
+    int solveTab(vector<vector<int>>&triangle){
+        int n =triangle.size();
+        int m = triangle.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i =n-1;i>=0;i--){
+            for(int j =i;j>=0;j--){
+                dp[i][j]  = triangle[i][j] + min(dp[i+1][j],dp[i+1][j+1]); 
+            }
+        }
+        return dp[0][0];
+    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n =triangle.size();
         int m = triangle.size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(triangle,0,0,dp);
+        // return solve(triangle,0,0,dp);
+        return solveTab(triangle);
 
     }
 };
