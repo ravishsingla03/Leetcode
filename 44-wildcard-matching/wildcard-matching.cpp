63b584e782748a729959002db510ee269c40103d
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2023-12-28 22:51:50 +0530

    Time: 55 ms (48.17%) | Memory: 27.9 MB (36.60%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    bool solve(string&s,string& p,int i,int j,vector<vector<int>>&dp){
+        if(i<0 && j<0){
+            return true;
+        }
+        if(i>=0 && j<0){
+            return false;
+        }
+        if(i<0 && j>=0){
+            for(int k=0;k<=j;k++){
+                if(p[k]!='*'){
+                    return false;
+                }
+            }
+            return true;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s[i]==p[j] || p[j]=='?'){
+            return dp[i][j]=solve(s,p,i-1,j-1,dp);
+        }
+        else if(p[j]=='*'){
+            return dp[i][j]=solve(s,p,i-1,j,dp) || solve(s,p,i,j-1,dp);
+        }
+        else{
+            return false;
+        }
+    }
+public:
+    bool isMatch(string s, string p) {
+        vector<vector<int>>dp(s.length(),vector<int>(p.length(),-1));
+        return solve(s,p,s.length()-1,p.length()-1,dp);
+    }
+};
