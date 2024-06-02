a85ba2f9a4e84f6e454968970993c8310254dc02
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2024-06-02 12:04:32 +0530

    Time: 5 ms (42.70%) | Memory: 14.2 MB (20.65%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,36 +1,33 @@
 class Solution {
-    map<vector<int>, bool> mp;
-    void solve(vector<int>&temp , vector<int>&c, int tar,int index){
-        if(tar==0){
-            if(!mp[temp])
-                mp[temp]=true;
+    void solve(vector<int>&c, int target , int index,vector<int>temp , set<vector<int>>&v){
+
+        if(target == 0){
+                v.insert(temp);
+                return;
         }
-        if(tar<0){
-            return;
-        }
-        if(index>c.size()-1){
-            return;
-        }
-        for(int i= index;i<c.size();i++){
-            if(c[i]>tar){
-                break;
-            }
-            if(i  > index && c[i] == c[i-1])
-                continue;
+        for(int i =index ;i<c.size();i++){
+            if(c[i]>target) break;
+            if(i>index && c[i]==c[i-1]) continue;
+
+
             temp.push_back(c[i]);
-            solve(temp,c,tar - c[i],i+1);
+            solve(c,target -c[i],i+1,temp,v);
             temp.pop_back();
         }
+
+        return ;
     }
 public:
     vector<vector<int>> combinationSum2(vector<int>& c, int target) {
         sort(c.begin(),c.end());
-        vector<int> temp;
-        solve(temp,c,target,0);
-        vector<vector<int>>v;
-        for(auto i:mp){
-            v.push_back(i.first);
+        set<vector<int>>v;
+        vector<int>temp;
+
+        solve(c,target , 0,temp,v);   
+        vector<vector<int>>ans;
+        for(auto i:v){
+            ans.push_back(i);
         }
-        return v;
+        return ans;
     }
 };
