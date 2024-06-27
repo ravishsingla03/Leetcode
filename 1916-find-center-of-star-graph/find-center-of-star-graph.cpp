480239fd43143778fcf3832e4578ff47a6e63268
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2024-06-27 23:17:07 +0530

    Time: 129 ms (80.03%) | Memory: 70.7 MB (92.91%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -3,19 +3,16 @@ public:
     int findCenter(vector<vector<int>>& edges) {
         int first =edges[0][0];
         int second =edges[0][1];
-        for(int i=1;i<edges.size();i++){
-            int x=edges[i][0];
-            int y=edges[i][1];
-            if(x==first){
-                return x;
-            }
-            else if(x==second){
-                return x;
-            }
-            else{
-                return y;
-            }
+        int x=edges[1][0];
+        int y=edges[1][1];
+        if(x==first){
+            return x;
         }
-        return -1;
+        else if(x==second){
+            return x;
+        }
+        else{
+            return y;
+        }
     }
 };
