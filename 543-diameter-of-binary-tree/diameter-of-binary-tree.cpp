08e251fe288a6463ce2179216a856615c3a0bcb2
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2024-07-16 20:23:00 +0530

    Time: 13 ms (54.30%) | Memory: 22.5 MB (12.07%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -10,31 +10,25 @@
  * };
  */
 class Solution {
-    void solve(TreeNode* root,int ans,int &maxans){
+    pair<int,int> solve(TreeNode* root){
         if(root==NULL){
-            return ;
-
-        }
-        if(!root->left && root->right==NULL){
-            maxans =max(ans,maxans);
-            return ;
+            return {0,0};
         }
 
-        solve(root->left,ans+1,maxans);
-        solve(root->right,ans+1, maxans);
+        pair<int,int>left =  solve(root->left);
+        pair<int,int>right =  solve(root->right);
+
+        int op1 = left.first;
+        int op2 = right.first;
+        int op3 = left.second + right.second + 1;
+
+        pair<int,int>ans;
+        ans.first = max(op1,max(op2,op3));
+        ans.second = max(left.second , right.second) +1;
+        return ans;
     }
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        if(!root){
-            return 0;
-        }
-
-        int op1 = diameterOfBinaryTree(root->left);
-        int op2 = diameterOfBinaryTree(root->right);
-        int maxans=0;
-        int maxans1=0;
-        solve(root->left,1,maxans);
-        solve(root->right,1,maxans1);
-         return max(op1,max(op2,maxans + maxans1));
+        return solve(root).first-1;
     }
 };
