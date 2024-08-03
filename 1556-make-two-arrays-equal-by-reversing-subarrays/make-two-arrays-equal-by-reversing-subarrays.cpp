a688bc739cbeb17ba0ab32b21acee2e8110ac74f
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2024-08-03 13:20:44 +0530

    Time: 11 ms (43.49%) | Memory: 17.5 MB (57.40%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -3,11 +3,10 @@ public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
         sort(target.begin(),target.end());
         sort(arr.begin(),arr.end());
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]!=target[i])
-                return false;
+
+            if(arr!=target) return false;
             
-        }    
+      
         return true;
     }
 };
