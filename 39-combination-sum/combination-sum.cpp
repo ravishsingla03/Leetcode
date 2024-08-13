9c976edb32a0ba6fcf9fa7ae6a043e59786fc35b
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2024-08-13 15:42:05 +0530

    Time: 48 ms (17.35%) | Memory: 42.4 MB (18.01%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,33 +1,27 @@
 class Solution {
-    void solve(vector<int>&c,int target,int index,int sum,vector<vector<int>>&v,vector<int>temp){
-        if(target == sum){
-            v.push_back(temp);
-            return ;
-        }
-        if(index >= c.size()){
-            return ;
-        }
-        if(target<sum){
+    void solve(vector<int>&c,int target,int index,vector<vector<int>>&ans,vector<int>temp){
+        
+        if(index==c.size()){
+            if(target==0){
+                ans.push_back(temp);
+            }
             return ;
         }
 
-
-        cout<<sum<<" ";
-
-        for(int i =index ; i<c.size();i++){
-            temp.push_back(c[i]);
-            solve(c,target,i,sum + c[i],v,temp);
+        if(c[index]<=target){
+            temp.push_back(c[index]);
+            solve(c,target-c[index],index,ans,temp);
             temp.pop_back();
         }
 
-        return ;
+        solve(c,target,index+1,ans,temp);
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& c, int target) {
-        vector<vector<int>>v;
+        vector<vector<int>>ans;
         vector<int>temp;
 
-        solve(c,target , 0 ,0 ,v , temp);
-        return v;
+        solve(c,target,0,ans,temp);
+        return ans;
     }
 };
