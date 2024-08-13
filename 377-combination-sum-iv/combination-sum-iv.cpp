cc6cea74e76779bfd75418d9c1a558631bf3157e
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2024-08-13 18:19:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (39.03%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,41 +1,23 @@
 class Solution {
-    int solve(vector<int>& nums, int tar,vector<int>&dp){
-        if(tar<0){
-            return 0;
-        }
-        if(tar==0){
+    int solve(vector<int>&nums,int target,vector<int>&dp){
+        if(target==0){
             return 1;
         }
-        if(dp[tar]!=-1){
-            return dp[tar];
+        if(target<0){
+            return 0;
+        }
+        if(dp[target]!=-1){
+            return dp[target];
         }
         int ans=0;
-        for(int i=0;i<nums.size();i++){
-            ans +=solve(nums,tar-nums[i],dp);
+        for(int i = 0 ; i<nums.size();i++){
+            ans+=solve(nums,target-nums[i],dp);
         }
-        dp[tar] = ans;
-        return dp[tar];
+        return dp[target]=ans;
     }
-
-    
-    int solveTab(vector<int>& nums, int tar){
-        vector<unsigned int>dp(tar+1,0);
-        dp[0]=1;
-        for(int i=1;i<=tar;i++){
-            for(int j=0;j<nums.size();j++){
-                if( i-nums[j] >=0){
-                    dp[i] += dp[i-nums[j]];
-                }
-            }
-        }
-        return dp[tar];
-    }
-
-    
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        // vector<int>dp(target+1,-1);
-        // return solve(nums,target,dp);
-        return solveTab(nums,target);
+        vector<int>dp(target+1,-1);
+        return solve(nums,target,dp);
     }
 };
