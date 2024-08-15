bb0ae836c95660b64c882b8b499a4e2b1bf88d52
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2024-08-15 21:32:48 +0530

    Time: 72 ms (57.53%) | Memory: 85.9 MB (50.86%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -4,29 +4,21 @@ public:
         int x=0;
         int y=0;
         for(auto i:bills){
-            if(i==5){
-                x++;
-            }
+            if(i==5) x++;
             else if(i==10){
-                y++;
                 if(x>0){
                     x--;
+                    y++;
                 }
-                else{
-                    return false;
-                }
+                else  return false;   
             }
             else{
                 if(x>0 && y>0){
                     x--;
                     y--;
                 }
-                else if(x>2){
-                    x= x-3;
-                }
-                else{
-                    return false;
-                }
+                else if(x>2) x= x-3;                
+                else return false;
             }
         }
         return true;
