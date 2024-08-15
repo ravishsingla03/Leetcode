d6a0abfc2c17421f7a5e590a7b170dce4e52a569
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2024-08-15 23:56:50 +0530

    Time: 270 ms (6.38%) | Memory: 16.7 MB (6.84%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,33 +1,14 @@
 class Solution {
-    // long long int solve(vector<int>&nums,int index){
-    //     if(index==nums.size()){
-    //         return 1;
-    //     }
-    //     long long int maxi =INT_MIN;
-    //     for(int i= index; i<nums.size();i++){
-    //         maxi = max(nums[i]*solve(nums,i+1),maxi);
-    //     }
-    //     return maxi;
-    // }
 public:
     int maxProduct(vector<int>& nums) {
-        double maxi = INT_MIN;
-        double prod=1;
-
-        for(int i=0;i<nums.size();i++)
-        {
-          prod*=nums[i];
-          maxi=max(prod,maxi);
-          if(prod==0)
-            prod=1;
-        }
-        prod =1;
-        for(int i=nums.size()-1;i>=0;i--)
-        {
-          prod*=nums[i];
-          maxi=max(prod,maxi);
-          if(prod==0)
-           prod=1;
+        double maxi =INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            double product = nums[i];
+            for(int j=i+1;j<nums.size();j++){
+                maxi = max(product,maxi);
+                product  = product * nums[j];
+            }
+            maxi = max(product,maxi);
         }
         return maxi;
     }
