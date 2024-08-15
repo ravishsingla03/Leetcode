4117f2da8a3af4a60e9a34c89bea7990e5d46144
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2024-08-15 21:30:10 +0530

    Time: 78 ms (30.17%) | Memory: 85.9 MB (50.86%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int x=0;
+        int y=0;
+        for(auto i:bills){
+            if(i==5){
+                x++;
+            }
+            else if(i==10){
+                y++;
+                if(x>0){
+                    x--;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                if(x>0 && y>0){
+                    x--;
+                    y--;
+                }
+                else if(x>2){
+                    x= x-3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+};
