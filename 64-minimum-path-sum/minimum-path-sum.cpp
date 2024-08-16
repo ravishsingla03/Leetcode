1bcf5b31f6c0c21e327735e41411243c681bbaf8
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2024-08-16 22:24:04 +0530

    Time: 7 ms (61.18%) | Memory: 13 MB (11.98%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -16,7 +16,36 @@ class Solution {
     }
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>>dp(grid.size(),vector<int>(grid[0].size(),-1));
-        return solve(0,0,grid,dp);
+        // return solve(0,0,grid,dp);
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+
+        for(int i = m-1;i>=0;i--){
+            for(int j = n-1;j>=0;j--){
+                if(i==m-1 && j==n-1) 
+                    dp[i][j] = grid[i][j];
+                else{
+                    int down = grid[i][j];
+                    if(i<m-1){
+                        down+= dp[i+1][j];
+                    }
+                    else{
+                        down+=1e9;
+                    }
+
+                    int right=grid[i][j];
+                    if(j<n-1){
+                        right+=dp[i][j+1];
+                    }
+                    else{
+                        right+=1e9;
+                    }
+                    dp[i][j] = min(down , right);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
