d105d887b035e2eb0f21d38b0f571c66e02d9896
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2024-08-16 22:35:43 +0530

    Time: 207 ms (6.41%) | Memory: 92.8 MB (5.01%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,26 +1,31 @@
 class Solution {
-    int solve(vector<int>&coins ,int sum,vector<int>&dp){
+    int solve(vector<int>&coins,int sum,int index,vector<vector<int>>&dp){
         if(sum==0){
             return 0;
         }
-        if(sum <0){
+        if(index==coins.size() || sum<0){
             return INT_MAX;
         }
-        if(dp[sum]!=-1){
-            return dp[sum];
+        if(dp[sum][index]!=-1){
+            return dp[sum][index];
         }
-        int ans = INT_MAX;
-        for(int i =0;i<coins.size();i++){
-            int x = solve(coins,sum - coins[i],dp);
-            if(x!=INT_MAX){
-                ans = min(ans,x+1);
-            }
+        int pick = solve(coins,sum - coins[index],index,dp);
+        if(pick!=INT_MAX){
+            pick = pick +1;
         }
-        return dp[sum]=ans;
+        int notp = 0 + solve(coins,sum,index+1,dp);
+        return dp[sum][index]=min(pick,notp);
+        
     }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,-1);
-        return solve(coins,amount,dp)!=INT_MAX ? solve(coins,amount,dp) : -1;
+        vector<vector<int>>dp(amount+1,vector<int>(coins.size(),-1));
+        int x =  solve(coins,amount,0,dp);
+        if(x!=INT_MAX){
+            return x;
+        }
+        else{
+            return -1;
+        }
     }
 };
