d1242d99dbb25812dab875c0dc2a08e1f84bb67b
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2024-09-03 20:36:18 +0530

    Time: 837 ms (5.00%) | Memory: 54.2 MB (5.64%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    bool solve(vector<int>&nums,int index,vector<int>&dp){
+        int n =nums.size();
+        if(index==n-1){
+            return true;
+        }
+        if(index>=n){
+            return false;
+        }
+        if(nums[index]==0){
+            return false;
+        }
+        if(dp[index]!=-1){
+            return dp[index];
+        }
+        bool ans = false;
+        for(int i=1;i<=nums[index];i++){
+            ans = ans || solve(nums,index+i,dp);
+            if(ans){
+                break;
+            }
+        }
+        return dp[index]=ans;
+    }
+public:
+    bool canJump(vector<int>& nums) {
+        vector<int>dp(nums.size()+1,-1);
+        return solve(nums,0,dp);
+    }
+};
