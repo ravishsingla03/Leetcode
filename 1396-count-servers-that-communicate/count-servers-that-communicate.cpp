9b35bf5bd290f9f76d0c08155cca00512a6ff132
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2025-01-23 17:28:50 +0530

    Time: 3 ms (55.91%) | Memory: 26.7 MB (59.58%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -4,37 +4,27 @@ public:
         int count=0;
         int n =grid.size();
         int m = grid[0].size();
+
         vector<int>row(n,0);
         vector<int>col(m,0);
-        int ans =0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]){
-                    int k = 0;
-                    int count=0;
-                    while(k<n && count<2){
-                        if(grid[k][j]){
-                            count++;
-                        }
-                        k++;
-                    }
-                    k=0;
-                    int count2 = 0;
-                    while(k<m && count2<2){
-                        if(grid[i][k]){
-                            count2++;
-                        }
-                        k++;
-                    }
-                    if(count==2 || count2==2){
-                        ans++;
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]){
+                    if(row[i]>1 || col[j]>1){
+                        count++;
                     }
                 }
             }
         }
-        // for(auto i: row){
-        //     cout<<i<<" ";
-        // }
-        return ans;
+        return count;
+        
     }
 };
