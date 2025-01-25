cf5c3fd54dd5d5e28201185824c6250ab7885854
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2025-01-25 16:02:21 +0530

    Time: 2 ms (7.31%) | Memory: 10.8 MB (21.53%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -11,9 +11,28 @@ class Solution {
 
         return dp[index]=max(pick,notpick);
     }
+    int solve1(vector<int>&nums){
+        vector<int>dp(nums.size()+1,0);
+
+        for(int i=0;i<nums.size();i++){
+            int pick = nums[i];
+            if(i>1){
+                pick+= dp[i-2];
+            }
+            int notpick=0;
+            if(i>0){
+                notpick+=dp[i-1];
+            }
+            dp[i]=max(pick,notpick);
+        }
+
+        return dp[nums.size()-1];
+    }
 public:
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        return solve(0,nums,dp);
+        // vector<int>dp(nums.size(),-1);
+
+        // return solve(0,nums,dp);
+        return solve1(nums);
     }
 };
