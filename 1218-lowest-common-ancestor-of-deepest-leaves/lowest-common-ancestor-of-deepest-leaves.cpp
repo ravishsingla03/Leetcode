866f19b0bd8226835a9cf39ebfb385280d80da9f
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2025-04-04 16:36:00 +0530

    Time: 12 ms (5.11%) | Memory: 30.4 MB (5.11%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    vector<TreeNode*> solve(TreeNode* root){
+        queue<TreeNode*>q;
+        q.push(root);
+        vector<TreeNode*>ls;
+
+        while(!q.empty()){
+            int size = q.size();
+            ls.clear();
+            for(int i=0;i<size;i++){
+                TreeNode* temp = q.front();
+                ls.push_back(temp);
+                q.pop();
+                if(temp->left){
+                    q.push(temp->left);
+                }
+                if(temp->right){
+                   q.push(temp->right);
+                }
+            }
+        }
+        return ls;
+    }
+
+    TreeNode* solve(vector<TreeNode*>leaf,TreeNode* root){
+        if(root==NULL){
+            return root;
+        }
+        for(int i =0;i<leaf.size();i++){
+            if(root==leaf[i]){
+                return root;
+            }
+        }
+
+        TreeNode* left = solve(leaf,root->left);
+        TreeNode* right = solve(leaf,root->right);
+        if(left==NULL){
+            return right;
+        }
+        else if(right==NULL){
+            return left;
+        }
+        else{
+            return root;
+        }
+    }
+
+public:
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+       vector<TreeNode*> leaf = solve(root);   
+       return solve(leaf,root);
+    }
+};
