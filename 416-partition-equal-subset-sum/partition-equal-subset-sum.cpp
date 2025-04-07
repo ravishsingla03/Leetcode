8a049607dfd9b7940b60fd26649eeb833f10e63b
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2025-04-07 17:34:19 +0530

    Time: 135 ms (41.05%) | Memory: 101.2 MB (16.79%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -26,7 +26,21 @@ public:
         if(sum%2!=0){
             return false;
         }
-        vector<vector<int>>dp(nums.size(),vector<int>(sum/2+1,-1));
-        return solve(nums,0,sum/2,dp);
+        
+        vector<vector<int>>dp(nums.size()+1,vector<int>((sum/2)+1,0));
+        for(int i=0;i<=nums.size();i++) dp[i][0]=1;
+
+        for(int i=nums.size()-1;i>=0;i--){
+            for(int j=0;j<=sum/2;j++){
+                int pick =0;
+                if(j>=nums[i]){
+                    pick =dp[i+1][j-nums[i]];
+                }
+                int notpick =dp[i+1][j];
+
+                dp[i][j]=pick | notpick;
+            }
+        }
+        return dp[0][sum/2];
     }
 };
