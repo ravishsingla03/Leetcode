7dff50a7467d340f117ff394c917d58961c1fffa
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2025-04-09 17:47:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (86.13%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -8,38 +8,48 @@ public:
         }
 
         int mp1[26]={0};
+        int mp2[26]={0};
+
         for(auto i: s1){
             mp1[i-'a']++;
         }
 
-        int mp2[26]={0};
         for(int i=0;i<n1;i++){
             mp2[s2[i]-'a']++;
         }
-        int flag=1;
+
+        int matches=26;
         for(int i=0;i<26;i++){
             if(mp1[i]!=mp2[i]){
-                flag=0;
+                matches--;
             }
         }
 
-        if(flag){
-            return flag;
+        if(matches==26){
+            return true;
         }
+        cout<<matches;
         int j =0;
         for(int i=n1;i<n2;i++){
             mp2[s2[j]-'a']--;
+            if(mp1[s2[j]-'a']==mp2[s2[j]-'a']){
+                matches++;
+            }
+            else if (mp1[s2[j]-'a']-1==mp2[s2[j]-'a']){
+                matches--;
+            }
             j++;
             mp2[s2[i]-'a']++;
-            int flag=1;
-            for(int k=0;k<26;k++){
-                if(mp1[k]!=mp2[k]){
-                   flag=0;
-                }
+            if(mp1[s2[i]-'a']==mp2[s2[i]-'a']){
+                matches++;
             }
-            if(flag){
-                return flag;
+            else if(mp1[s2[i]-'a']+1==mp2[s2[i]-'a']){
+                matches--;
             }
+            if(matches==26){
+                return true;
+            }
+
         }
         
         return false;
