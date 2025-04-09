05fef11c868e695c8e648384c214f00012230bef
Ravish Singla <120041859+ravishsingla03@users.noreply.github.com>
2025-04-09 17:27:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (67.11%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,60 +1,47 @@
-
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution
-{
-private:
-    bool check(int ar1[26], int ar2[26])
-    {
-        for (int i = 0; i < 26; i++)
-        {
-            if (ar1[i] != ar2[i])
-            {
-                return 0;
-            }
-        }
-        return 1;
-    }
-
+class Solution {
 public:
-    bool checkInclusion(string s1, string s2)
-    {
-        int count1[26] = {0};
-        // store count
-        for (int i = 0; i < s1.size(); i++)
-        {
-            int index = s1[i] - 'a';
-            count1[index]++;
-        }
-        int count2[26] = {0};
-        int i = 0;
-
-        while (i < s1.size() && i<s2.size())
-        {
-            int index = s2[i] - 'a';
-            count2[index]++;
-            i++;
+    bool checkInclusion(string s1, string s2) {
+        int n1 = s1.size();
+        int n2 = s2.size();
+        if(n1>n2){
+            return false;
         }
 
-        if (check(count1, count2))
-        {
-            return 1;
+        int mp1[26]={0};
+        for(auto i: s1){
+            mp1[i-'a']++;
         }
 
-        while (i < s2.size())
-        {
-            int index = s2[i] - 'a';
-            count2[index]++;
-            char oldchar = s2[i-s1.size()];
-            index =oldchar - 'a';
-            count2[index]--;
-            if (check(count1, count2))
-            {
-                return 1;
+        int mp2[26]={0};
+        for(int i=0;i<n1;i++){
+            mp2[s2[i]-'a']++;
+        }
+        int flag=1;
+        for(int i=0;i<26;i++){
+            if(mp1[i]!=mp2[i]){
+                flag=0;
             }
-            i++;
         }
-        return 0;
+
+        if(flag){
+            return flag;
+        }
+        int j =0;
+        for(int i=n1;i<n2;i++){
+            mp2[s2[j]-'a']--;
+            j++;
+            mp2[s2[i]-'a']++;
+            int flag=1;
+            for(int k=0;k<26;k++){
+                if(mp1[k]!=mp2[k]){
+                   flag=0;
+                }
+            }
+            if(flag){
+                return flag;
+            }
+        }
+        
+        return false;
     }
 };
